2bf2ee74134650da716bb1be9194be2a404531b9
Hubert Gruniaux <hubert@gruniaux.com>
2021-02-03 17:31:03 +0100

    Use CMake to retrieve the project version instead of hardcoding it in the code

--- sources/config.hpp
+++ sources/config.hpp
@@ -22,23 +22,12 @@
 // SOFTWARE.
 //
 
-#include "mainwindow.hpp"
-#include "config.hpp"
-#include <QApplication>
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    a.setApplicationDisplayName("Watermark");
-    a.setApplicationName("watermark");
-    a.setApplicationVersion(VERSION_STRING);
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 2
+#define VERSION_PATCH 0
+#define VERSION_STRING "1.2.0"
 
-    MainWindow w;
-    w.showMaximized();
-
-    if(a.arguments().size() > 1) {
-        w.loadImage(a.arguments()[1]);
-    }
-
-    return a.exec();
-}
+#endif /* !CONFIG_HPP */
